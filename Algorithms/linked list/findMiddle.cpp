690f5f8c2b9a041d2814f5f30c2477b2c478af1c
Ishika Mitra <30717395+i-m-ishika@users.noreply.github.com>
2020-10-01 01:27:00 +0530

    Update findMiddle.cpp

--- Algorithms/linked list/findMiddle.cpp
+++ Algorithms/linked list/findMiddle.cpp
@@ -11,7 +11,8 @@ struct node{
 node* findMiddle(node *head){
   //1 -> 2 -> 3 -> 4 -> 5 return 3
   //11 -> 12 -> 13 -> 14 return 12
-
+  if(!head)
+    return head;
   node *fast = head, *slow = head;
   while(1){
     fast = fast -> next;
