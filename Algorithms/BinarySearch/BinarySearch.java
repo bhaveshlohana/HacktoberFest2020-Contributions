aa02aaf43e466abf04e27154d0b61c83d523a91f
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-10-01 20:09:02 +0530

    organised

--- Algorithms/BinarySearch/BinarySearch.java
+++ Algorithms/BinarySearch/BinarySearch.java
@@ -1,43 +1,25 @@
-
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. 
-    // r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        if (r >= l) { 
-            int mid = l + (r - l) / 2; 
-  
-            // If the element is present at the 
-            // middle itself 
-            if (arr[mid] == x) 
-                return mid; 
-  
-            // If element is smaller than mid, then 
-            // it can only be present in left subarray 
-            if (arr[mid] > x) 
-                return binarySearch(arr, l, mid - 1, x); 
-  
-            // Else the element can only be present 
-            // in right subarray 
-            return binarySearch(arr, mid + 1, r, x); 
-        } 
-  
-        // We reach here when element is not present 
-        // in array 
-        return -1; 
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
-        else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+class BinarySearchExample{  
+    public static void binarySearch(int arr[], int first, int last, int key){  
+      int mid = (first + last)/2;  
+      while( first <= last ){  
+         if ( arr[mid] < key ){  
+           first = mid + 1;     
+         }else if ( arr[mid] == key ){  
+           System.out.println("Element is found at index: " + mid);  
+           break;  
+         }else{  
+            last = mid - 1;  
+         }  
+         mid = (first + last)/2;  
+      }  
+      if ( first > last ){  
+         System.out.println("Element is not found!");  
+      }  
+    }  
+    public static void main(String args[]){  
+           int arr[] = {10,20,30,40,50};  
+           int key = 30;  
+           int last=arr.length-1;  
+           binarySearch(arr,0,last,key);     
+    }  
+   }  
---
+++ Algorithms/ip.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  system ("C:\\Windows\\System32\\ipconfig");
+  return 0;
+}
