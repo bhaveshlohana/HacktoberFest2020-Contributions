e5b4db18cfb458cc5749a7ab172efadb73c848a7
gauravmadan583 <54912404+gauravmadan583@users.noreply.github.com>
2020-10-01 11:03:43 +0530

    Binary and Linear Search added for Python

---
+++ Algorithms/Linear-search/linear_search.py
@@ -0,0 +1,20 @@
+n = int(input("Enter the number of elements in array: "))
+a = []
+print('Enter {} elements: '.format(n))
+for i in range(n):
+    x = int(input())
+    a.append(x)
+
+key = int(input("Enter the key you would do search: "))
+
+found = False
+index = -1
+for i in range(n):
+    if a[i] == key:
+        found = True
+        index = i
+        break
+if found:
+    print('Key {} found at index {}'.format(key, index))
+else:
+    print('Key {} not found in the array'.format(key))
