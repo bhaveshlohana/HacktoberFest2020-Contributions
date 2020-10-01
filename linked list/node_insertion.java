4160bc6b3b8cb2eec5fc071a5fc91b490a623c6e
Pavan Kommisetty <52818671+Kazeno21@users.noreply.github.com>
2020-10-01 09:34:00 +0530

    node_insertion in java (code snippet)

---
+++ linked list/node_insertion.java
@@ -0,0 +1,15 @@
+// Linked List Class 
+class LinkedList 
+{ 
+    Node head;  // head of list 
+  
+    /* Node Class */
+    class Node 
+    { 
+        int data; 
+        Node next; 
+           
+        // Constructor to create a new node 
+        Node(int d) {data = d; next = null; } 
+    } 
+}
