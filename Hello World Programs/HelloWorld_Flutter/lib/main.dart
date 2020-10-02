a6b08f780a9b3bece7ca6640f2c83ee7fcdde716
Shubham Rattra <46563834+ShubhamRattra@users.noreply.github.com>
2020-10-02 10:43:38 +0530

    Add files via upload

---
+++ Hello World Programs/HelloWorld_Flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  );
+}
