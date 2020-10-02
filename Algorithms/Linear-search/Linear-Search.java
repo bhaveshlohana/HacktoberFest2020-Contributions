583aa5b4f9fcf20cfa18a23a3d32dbaa9b8e1204
Ridam Hazra <rhazra0602@gmail.com>
2020-10-02 14:07:07 +0530

    Update Linear-Search.java
    added linear search algo in JAVA

--- Algorithms/Linear-search/Linear-Search.java
+++ Algorithms/Linear-search/Linear-Search.java
@@ -1,84 +1,29 @@
-import java.util.Scanner;
+// Java code for linearly searching x in arr[]. If x 
+// is present then return its location, otherwise 
+// return -1 
 
+class GFG 
+{ 
+public static int search(int arr[], int x) 
+{ 
+	int n = arr.length; 
+	for(int i = 0; i < n; i++) 
+	{ 
+		if(arr[i] == x) 
+			return i; 
+	} 
+	return -1; 
+} 
 
-/**
- * 
- * 
- * Linear Search scans each elements in the array 
- * 
- * Time Complexity:-
- * 		Best Case:- element present at the start of the element 
- * 					O(1)
- * 		
- * 		Worst Case :- element not present in the list
- * 					O(n)
- *
- * Space Complexity:-  O(1) Constant Space
- */
-public class LinearSearch {
-
-	public static int search(int[] arr, int key) {
-
-		for(int i=0;i<arr.length;i++)
-		{
-			if(arr[i] == key)
-				return i;
-		}
-		
-		return -1;
-	}
-}
-
-
-class LinearSearchImplementation {
+public static void main(String args[]) 
+{ 
+	int arr[] = { 2, 3, 4, 10, 40 }; 
+	int x = 10; 
 	
-	public static void main(String[] args) {
-
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the number of Elements");
-		
-		int n=s.nextInt();
-		int arr[]=new int[n];
-		
-		System.out.println("Enter the Elements");
-		for(int i=0;i<n;i++)
-			arr[i]=s.nextInt();
-		
-		System.out.print("Enter the Element to be searched :");
-		int key=s.nextInt();
-	
-		int result = LinearSearch.search(arr,key);
-		
-		if(result == -1)
-			System.out.println("The element is not present in list");
-		else
-			System.out.print("The element is present at :"+ (result+1)+" position" );
-
-	}
-	
-}
-
-/*
- * 
-Enter the number of Elements
-5
-Enter the Elements
-1 2 10 9 3
-Enter the Element to be searched :10
-The element is present at :3 position
-
-
-Enter the number of Elements
-5
-Enter the Elements
-1 2 10 9 3
-Enter the Element to be searched :7
-The element is not present in list
-
-
-
-
-
-
-*/
-
+	int result = search(arr, x); 
+	if(result == -1) 
+		System.out.print("Element is not present in array"); 
+	else
+		System.out.print("Element is present at index " + result); 
+} 
+} 
