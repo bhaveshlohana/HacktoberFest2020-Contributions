c10b65c725c9040f57ceef7ec9d40d1ddaa3c441
schnitzel4 <mhschnitzel4@googlemail.com>
2020-10-04 00:13:48 +0200

    add hello worl version

---
+++ Hello World Programs/Python/Hello_World_different_lang.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+
+def main() -> None:
+    print("Hello World")
+    print("Hallo Welt")
+    print("Hola Mundo")
+    print("Bonjour le monde")
+    print("Hej Verden")
+
+
+if __name__ == "__main__":
+    main()
