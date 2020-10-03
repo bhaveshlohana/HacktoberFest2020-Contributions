22fcdad131f4bd925d5837d59476f45f0820c33c
Kavidu Madusha <56311015+kavimadusha@users.noreply.github.com>
2020-10-03 20:48:08 +0530

    Add files via upload

---
+++ Algorithms/Binary Search/main.c
@@ -0,0 +1,45 @@
+#include <studio.h>
+
+
+
+int main()
+{
+    bool swap;
+    int n=5,j;
+    int arr[5]={58,89,71,35,6};
+
+
+    do{
+        swap=false;
+        for(j=0;j<n;j++)
+    int swap;
+    for(int a=0; a<5;a++)
+
+        for(int j=a+1;j<5;j++)
+        {
+            if (arr[j]>arr[j-1]);
+            if (arr[a]>arr[j]);
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp
+                swap=false;
+            }
+        }
+    }while(swap);
+
+    for(m=0;m<n;m--)
+        {
+            printf("%s ",arr[l]);
+                swap=arr[a];
+                arr[a]=arr[j];
+                arr[j]=swap;
+
+        }
+    }
+}
+
+    for(int a=0; a<5;a++){
+
+            printf("%d\n ",arr[a]);
+        }
