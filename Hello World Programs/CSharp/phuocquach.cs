00df5036c31a0a10686358dd8ae5d82ccd3d959d
Phuoc Quach ngoc <phuocquach.nt@gmail.com>
2020-10-04 11:56:26 +0700

    add hello world

---
+++ Hello World Programs/CSharp/phuocquach.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!, My Name is Phuoc");
+        }
+    }
+}
