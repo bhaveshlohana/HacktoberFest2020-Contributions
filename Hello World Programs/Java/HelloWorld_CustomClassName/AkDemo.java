4c13123bbdea85ed29508934f7f80f279a74bfee
Akshat Agrawal <49070489+akshatagrawal9874@users.noreply.github.com>
2020-10-05 11:36:06 +0530

    print HelloWorld in Java using Custom ClassName
    Print Hello World using my Name instead of System Class

---
+++ Hello World Programs/Java/HelloWorld_CustomClassName/AkDemo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class AkDemo
+{
+	public static void main(String args[])
+	{
+                                  // Using my Class Name
+		Akshat.ak.println("Hello World");
+	}
+}
