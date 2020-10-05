551aaa0580cd9677619422c708726d5d2c1fee16
shwetankverma13 <55885588+shwetankverma13@users.noreply.github.com>
2020-10-05 23:53:26 +0530

    updated seive
    last algo time complexity was t*NloglogN...
    This algo has time complexity NloglogN +t as each query is addressed in O(1) time...Its just improvisation to previous code

--- Algorithms/Sieve of Eratosthenes/SieveofEratosthenes.java
+++ Algorithms/Sieve of Eratosthenes/SieveofEratosthenes.java
@@ -3,21 +3,23 @@ import java.util.*;
 import java.lang.*;
 
 class SieveofEratosthenes {
+	static boolean[] primenum = new boolean[1000007];
 	public static void main (String[] args) {
+		prime(1000000);
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		while(t-- > 0){
 		    int n = sc.nextInt();
-		    prime(n);
+		    System.out.println(primenum[n]);
 		}
 	}
 	
 	public static void prime(int n){
-	    boolean[] primenum = new boolean[n+1];
+	    
 	    for(int i = 0; i<n; i++){
 	        primenum[i] = true;
 	    }
-	    
+	    primenum[0]=primenum[1]=false;
 	    for(int i = 2; i*i <= n; i++){
 	        if( primenum[i] == true ){
 	            for(int j = i*i; j<=n; j+= i){
@@ -26,11 +28,6 @@ class SieveofEratosthenes {
 	        }
 	    }
 	    
-	    for(int i = 2; i <= n; i++){
-	        if( primenum[i] ){
-	            System.out.print(i + " ");
-	        }
-	    }
-	    System.out.println();
+	    
  	}
 }
