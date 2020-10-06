db3a7de084026203ef6af40914ee8e383679cbe5
Timothy Leonard <timothyleonard@Timothys-MBP.home>
2020-10-06 10:33:59 -0400

    add return statement

--- Algorithms/js-primes-sieve.js
+++ Algorithms/js-primes-sieve.js
@@ -1,15 +1,17 @@
 const primesSieve = function(num){
-    const primes = [];
+    let primes = [];
     for(let i = 2; i<=num; i++){
         primes.push(i);
     };
     for(let j = 2; j<=Math.sqrt(num); j++){
         if (primes.includes(j)){
             for(let k = j*j; k <= num; k+=j){
-                console.log(`removing ${k}`)
-                primes = primes.splice(primes.indexOf(k), 1)
-            }
-        }
-    }
-    console.log(primes)
-}
+              if (primes.includes(k)){
+                primes.splice(primes.indexOf(k), 1);
+                };  
+            };
+        };
+    };
+    console.log(primes);
+    return primes;
+};
