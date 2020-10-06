13fc3160840bc0d55aaec89c48ac4e0a578b07f9
Adam Jones <adam@ayeteejay.com>
2020-10-06 13:57:18 -0700

    Refactoring linear search

--- Algorithms/Linear-search/Linear-Search.js
+++ Algorithms/Linear-search/Linear-Search.js
@@ -1,12 +1,14 @@
-function linearSearch(array, element) {
-  for (var i=0; i<array.length; i++) {
-    if (arr[i] == element) {
-      return i;
-    }
-  } return null;
-}
+const array = [1, 2, 4, 5, 7, 8, 9, 2];
 
-var array = [1,2,4,5,7,8,9,2];
+// Match all using .filter
+const filterSearch = (arr, elem) => arr.filter((val) => elem == val);
 
-linearSearch(array, 0); // returns null
-linearSearch(array, 2); // returns 1
+// Check for element using .includes
+const includesSearch = (arr, elem) => (arr.includes(elem) ? true : false);
+
+// Check for element using .indexOf
+const indexOfSearch = (arr, elem) => (arr.indexOf(elem) !== -1 ? true : false);
+
+// Check for element using .find
+const findSearch = (arr, elem) =>
+  arr.find((val) => val === elem) ? true : false;
