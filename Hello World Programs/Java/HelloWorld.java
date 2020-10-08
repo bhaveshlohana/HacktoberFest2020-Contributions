386d2f2c2af4e21cbbc19c12301e272a8c2e1c62
IryaDev <67646879+IryaDev@users.noreply.github.com>
2020-10-08 14:36:51 +0200

    Add files via upload

--- Hello World Programs/Java/HelloWorld.java
+++ Hello World Programs/Java/HelloWorld.java
@@ -1,5 +1,13 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+/** 
+    This is my first program!
+
+    @author IryaDev
+*/
+
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World"); 
+    public static void main (String[] args){
+        System.out.println("Hello World! :D");
     }
 }
