6ee2777559fd5319614bfbad84d76b99403ec1ad
Saloni Gupta <saloni.gupta63@gmail.com>
2020-10-09 12:52:37 +0530

    Create decreasingBinarySearch.cpp

---
+++ Algorithms/Binary Search/decreasingBinarySearch.cpp
@@ -0,0 +1,36 @@
+//BINARY SEARCH FOR DECREASING ORDER
+
+#include <stdio.h>
+
+void main()
+{int a[10], i,n, elemm, beg,mid, end, flag;
+
+cout<< "enter number of elements:";
+cin>> n;
+cout<<"enter elements in decreasing order:";
+for(i=0; i<n;i++)
+{cin>> a[i];
+}
+
+cout<< "enter element to be searched:";
+cin>> elem;
+
+flag=0;
+while (beg< end)
+  mid= (beg+ end)/2
+{ if (a[mid]==elem}
+    cout<< mid;
+    flag=1;
+    break;
+    
+  else if (a[mid]< elem)
+    end=mid;
+    
+ else 
+  beg=mid;
+}  
+
+if (flag==0)
+{cout<< "elem not in list!";
+
+}
