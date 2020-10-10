ce0c036b1b2de8d1713d799997b99cbad07debff
Rutuja Dicholkar <44153792+Rutujavd@users.noreply.github.com>
2020-10-10 20:05:15 +0530

    Add files via upload

---
+++ Hello World Programs/Python/Rutujavd.py
@@ -0,0 +1,5 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> 
