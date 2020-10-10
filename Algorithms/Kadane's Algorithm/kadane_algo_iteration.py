2aa56877c1a8d7b50fc4bfb1c918ac75dda499ce
Stealth-py <52747708+Stealth-py@users.noreply.github.com>
2020-10-11 00:53:26 +0530

    Create kadane_algo_iteration.py
    A simple program which calculates the sum of a contiguous subarray using kadane's algorithm in an iteration version in python.
    Will also upload a dp version if you are interested...

---
+++ Algorithms/Kadane's Algorithm/kadane_algo_iteration.py
@@ -0,0 +1,19 @@
+#Find maximum sum of a contiguous subarray using Kadane's Algorithm in Python using  iteration
+
+from sys import maxsize       #importing sys library for MAX_INT
+
+def kadane(array, size):              
+	result = -maxsize        
+	max_here = 0
+	for i in range(size):
+		max_here+=array[i]
+		if result<max_here:                      #check when to change the result
+			result = max_here                      #change the result to the maximum sum yet
+		if max_here<0:
+			max_here = 0
+	return result
+
+
+array = [-2, -3, 4, -1, -2, 1, 5, -3] 
+size = len(array)
+print(kadane(array, size))
