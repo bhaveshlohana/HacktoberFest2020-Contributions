156a28cb248161a71d728dd82fd8376ada42f4a5
Jean de Lima Lopes <jean.llopes@outlook.com>
2020-10-11 22:07:01 +0000

    Add File HelloWorldFromJean.cs

---
+++ Hello World Programs/C#/HelloWorldFromJean.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldFromJean
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Jean...");
+            Console.ReadKey();
+        }
+    }
+}
