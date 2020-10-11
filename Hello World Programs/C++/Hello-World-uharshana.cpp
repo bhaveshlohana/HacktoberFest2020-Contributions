3e05c37d5d292a4c77f14b9fb6ee67ae91ebb6e2
Uvindu Harshana <64182114+d-folt@users.noreply.github.com>
2020-10-11 09:43:56 +0530

    Create Hello-World-uharshana.cpp

---
+++ Hello World Programs/C++/Hello-World-uharshana.cpp
@@ -0,0 +1,10 @@
+// AUTHOR: Uvindu Harshana
+// LANGUAGE: CPP
+// GITHUB: https://github.com/d-folt
+
+#include<iostream>;
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!"<<endl;
+    return 0;
