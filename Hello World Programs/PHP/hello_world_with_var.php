2472ed847b38cdc1149b4ef709faf1341121d90a
Muhammad Rizaldi <mrizaldi1998@gmail.com>
2020-10-12 10:19:31 +0800

    add hello world php with variable

---
+++ Hello World Programs/PHP/hello_world_with_var.php
@@ -0,0 +1,16 @@
+<?php
+$name = "udin";
+$message = "hello world!";
+?>
+
+<!DOCTYPE html>
+<html>
+<title></title>
+
+<body>
+
+    <h1>Hi , <?php echo $name; ?></h1>
+    <?php echo $message; ?> == <?php echo "hellow world!"; ?>
+</body>
+
+</html>
