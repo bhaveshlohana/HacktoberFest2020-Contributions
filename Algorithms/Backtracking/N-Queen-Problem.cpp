8e18d6cde3295a38e2d5418526bd375e2d51da84
Satya prakash <60463917+Spider-50140@users.noreply.github.com>
2020-10-14 00:27:29 +0530

    Update N-Queen-Problem.cpp

--- Algorithms/Backtracking/N-Queen-Problem.cpp
+++ Algorithms/Backtracking/N-Queen-Problem.cpp
@@ -1,75 +1,120 @@
-#include <bits/stdc++.h> 
+Problem Statement:-
+	N-Queen Problem
+        You are given N, and for a given N x N chessboard, find a way to place N queens such that no queen can attack any other queen on the chess board. 
+	A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. You have to print all such configurations.
+Input Format :
+Line 1 : Integer N
+Output Format :
+One Line for every board configuration. 
+Every line will have N*N board elements printed row wise and are separated by space
+Note : Don't print anything if there isn't any valid configuration.
+Constraints :
+1<=N<=10
+Sample Input 1:
+4
+Sample Output 1 :
+0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 
+0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0 
+
+
+
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long 
-# define N 8
-
-int n;
-
-bool isValid(std::vector<std::vector<int>> &board,int row,int col)
+    //To print the matrix
+void printer(int** arr, int n)
 {
-    //check if a queen is in left
-    for(int i=0;i<col;i++)
-    {
-        if(board[row][i])
-          return false;
-    }
-    //check if a queen is in upper left diagnol
-    for(int i=row, j=col; i>=0 and j>=0; i--,j--)
-    {
-        if(board[i][j])
-          return false;
-    }
-    //check if a queen is in lower left diagnol
-    for(int i=row, j=col; j>=0 and i<n; i++,j--)
-    {
-        if(board[i][j])
-          return false;
-    }
-    return true;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+	}
+    cout << endl;
 }
-
-bool solveNQueen(std::vector<std::vector<int>> &board, int col)
+  // To check that whether i can put queen at at particular index of row and coloumn or not
+bool checker(int **arr,int row,int col,int n)
 {
-    if(col>=n)
-       return true;
-    for(int i=0;i<n;i++)
+
+// Checking Same Column
+    for(int i=row-1;i>=0;i--)
     {
-        if(isValid(board,i,col))
+        if(arr[i][col] == 1)
         {
-            board[i][col]=1;
-            if(solveNQueen(board,col+1))
-              return true;
-            board[i][col]=0;
+            return false;
         }
     }
-    return false;
+//Checking Upper Left Diagonal
+    for(int i=row-1,j=col-1;i>=0 && j>=0 ; i--,j--)
+    {
+        if(arr[i][j] ==1)
+        {
+            return false;
+        }
+    }
+
+  // Checking Upper Right Diagonal
+
+    for(int i=row-1,j=col+1;i>=0 && j<n ; i--,j++)
+    {
+        if(arr[i][j] == 1)
+        {
+            return false;
+        }
+    }
+    // If none of above is an obstacle then we should return true 
+  return true;
+}
+       // To place all the queens
+void placer(int** arr, int row, int n)
+{
+	// If the row equals to n it means we have filled all the rows and reached to n so
+	// time to print the output by calling or print function we made on top
+	if (row == n)
+	{
+		printer(arr, n);
+		return;
+	}
+	// for each coloumn we are passing rows and checking all possible arrangements.
+    for(int j=0;j<n;j++)
+    {
+        if(checker(arr,row,j,n))
+        {
+            arr[row][j] = 1;
+            placer(arr,row+1,n);
+		// when we will return to this position time to make it 0 and move to next coloumn and check out remaining possiblity.
+            arr[row][j] = 0;
+        }
+    }
+    return;
+}
+void placeNQueens(int n)
+{
+	// Declared a 2-d Array to store all the correct positions and initialised it with zero
+	int** arr = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	// Called a Helper function by passing it our 2-d array and n.
+     placer(arr, 0, n); 
+	// Deleted the 2-D array we made to release all the occupied space.
+    for(int i=0; i<n; i++)
+    {
+        delete[]arr[i];
+    }
+    delete[]arr;
+}
+int main()
+{
+
+  int n; 
+  cin >> n ;
+  placeNQueens(n);
+
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int size;
-	cin>>size;
-	n=size;
-//	int board[size][size];
-	std::vector<std::vector<int>> board(size,std::vector<int>(size));
-	for(int i=0;i<size;i++)
-	 for(int j=0;j<size;j++)
-	   board[i][j]=0;
-	if(solveNQueen(board,0)==false)
-	{
-	    cout<<"No solution exist.";
-	}
-	else
-	{
-	    for(int i=0;i<n;i++)
-	      {
-	          for(int j=0;j<n;j++)
-	        cout<<board[i][j]<<"\t";
-	      cout<<endl;
-	      }
-	}
-	
-	
-	return 0;
-}
