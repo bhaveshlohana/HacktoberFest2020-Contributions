007c64053ab98d8034294f7380b0cbe2dd0a9d02
Manan-Sood <68143035+Manan-Sood@users.noreply.github.com>
2020-10-13 13:30:52 +0530

    Create Hello_World_New.cpp
    Hello world using pointers

---
+++ Hello_World_New.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    char *p = "Hello, World!";
+    
+    for (int i = 0; i <= 13; i++)
+        printf("%c", *(p+i));
+    
+    printf("\n");
+    
+    return 0;
+}
