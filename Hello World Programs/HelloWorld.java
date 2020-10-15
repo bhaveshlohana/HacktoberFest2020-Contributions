aa41d0514086035219672af9ab549fb028cfedb3
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2020-10-15 03:39:43 +0200

    Update HelloWorld.java

--- Hello World Programs/HelloWorld.java
+++ Hello World Programs/HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
+    
+    //main method to execute program functionality
     public static void main(String[] args) {
+        
+        //Print out Hello, World!
         System.out.println("Hello, World!"); 
     }
 }
