3eeb0c8bd9f0eaca01223370012ab2bd2c7fa19b
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2020-10-15 03:51:27 +0200

    Add files via upload

--- Hello World Programs/HelloWorld.java
+++ Hello World Programs/HelloWorld.java
@@ -1,9 +1,26 @@
-class HelloWorld {
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Oarabile Mwiya)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
     
-    //main method to execute program functionality
-    public static void main(String[] args) {
-        
-        //Print out Hello, World!
-        System.out.println("Hello, World!"); 
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloWorld()
+    {
+        // initialise instance variables
+       
+    }
+
+   public static void main (String [] args) {
+    //Output Hello world
+    System.out.println("Hello world");
     }
 }
