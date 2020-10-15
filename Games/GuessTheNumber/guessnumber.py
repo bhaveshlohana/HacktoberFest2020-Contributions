c55d4aa0b4f45df4089edbbdcac8c1bbf641fd5b
Abhi947223 <72922788+Abhi947223@users.noreply.github.com>
2020-10-15 21:47:56 +0530

    Update guessnumber.py

--- Games/GuessTheNumber/guessnumber.py
+++ Games/GuessTheNumber/guessnumber.py
@@ -1,6 +1,8 @@
 # Author : Daniele Carvalho
 # GitHub : daniele-mc
 import random
+
+# Guess from 100 numbers 
 guess = random.randrange(100)
 num = int(input("Enter a number (0 to 100): "))
 while (guess < 101):
