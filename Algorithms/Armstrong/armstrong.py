3eeceffb2a26a30c5fc00f04b11e9d72a67fd5c4
Khwab-kalra <58007063+Khwab-kalra@users.noreply.github.com>
2020-10-16 16:20:50 +0530

    Update armstrong.py
    Armstrong number not always is the cube of digits, power depends on the number of digits

--- Algorithms/Armstrong/armstrong.py
+++ Algorithms/Armstrong/armstrong.py
@@ -3,15 +3,22 @@ def isArmstrong(num):
   a=num
   while num>0:
       digit=num%10
-      sum=sum+pow(digit,3)
+      sum=sum+pow(digit,noOfDigits(a))
       num=num//10
   if sum==a:
       return True
   else:
       return False 
-    
+def noOfDigits(n) :
+  c=0
+  while n > 0:
+      digit = n %10
+      c=c+1 
+      n = n //10
+  return c
+
 if __name__ == "__main__":
-    n=int(input("enter the number"))
+    n=int(input("enter the number :"))
 
     if isArmstrong(n):
         print(f"{n} is Armstrong")
