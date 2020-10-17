30affba8bf2049e624c607e3c0aa744cc9be55d0
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2020-10-17 00:34:39 -0300

    Created binaria with Binary Tree Algorithm

---
+++ Algorithms/binaria/ArvoreBinaria.java
@@ -0,0 +1,238 @@
+/**
+ * No da arvore binaria
+ * @author Max do Val Machado
+ */
+class No {
+	public int elemento; // Conteudo do no.
+	public No esq, dir;  // Filhos da esq e dir.
+
+	/**
+	 * Construtor da classe.
+	 * @param elemento Conteudo do no.
+	 */
+	public No(int elemento) {
+		this(elemento, null, null);
+	}
+
+	/**
+	 * Construtor da classe.
+	 * @param elemento Conteudo do no.
+	 * @param esq No da esquerda.
+	 * @param dir No da direita.
+	 */
+	public No(int elemento, No esq, No dir) {
+		this.elemento = elemento;
+		this.esq = esq;
+		this.dir = dir;
+	}
+}
+
+/**
+ * Arvore binaria de pesquisa
+ * @author Max do Val Machado
+ */
+public class ArvoreBinaria {
+	private No raiz; // Raiz da arvore.
+
+	/**
+	 * Construtor da classe.
+	 */
+	public ArvoreBinaria() {
+		raiz = null;
+	}
+
+	/**
+	 * Metodo publico iterativo para pesquisar elemento.
+	 * @param x Elemento que sera procurado.
+	 * @return <code>true</code> se o elemento existir,
+	 * <code>false</code> em caso contrario.
+	 */
+	public boolean pesquisar(int x) {
+		return pesquisar(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para pesquisar elemento.
+	 * @param x Elemento que sera procurado.
+	 * @param i No em analise.
+	 * @return <code>true</code> se o elemento existir,
+	 * <code>false</code> em caso contrario.
+	 */
+	private boolean pesquisar(int x, No i) {
+      boolean resp;
+		if (i == null) {
+         resp = false;
+
+      } else if (x == i.elemento) {
+         resp = true;
+
+      } else if (x < i.elemento) {
+         resp = pesquisar(x, i.esq);
+
+      } else {
+         resp = pesquisar(x, i.dir);
+      }
+      return resp;
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void mostrarCentral() {
+		System.out.print("[ ");
+		mostrarCentral(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void mostrarCentral(No i) {
+		if (i != null) {
+			mostrarCentral(i.esq); // Elementos da esquerda.
+			System.out.print(i.elemento + " "); // Conteudo do no.
+			mostrarCentral(i.dir); // Elementos da direita.
+		}
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void mostrarPre() {
+		System.out.print("[ ");
+		mostrarPre(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void mostrarPre(No i) {
+		if (i != null) {
+			System.out.print(i.elemento + " "); // Conteudo do no.
+			mostrarPre(i.esq); // Elementos da esquerda.
+			mostrarPre(i.dir); // Elementos da direita.
+		}
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void mostrarPos() {
+		System.out.print("[ ");
+		mostrarPos(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void mostrarPos(No i) {
+		if (i != null) {
+			mostrarPos(i.esq); // Elementos da esquerda.
+			mostrarPos(i.dir); // Elementos da direita.
+			System.out.print(i.elemento + " "); // Conteudo do no.
+		}
+	}
+
+
+	/**
+	 * Metodo publico iterativo para inserir elemento.
+	 * @param x Elemento a ser inserido.
+	 * @throws Exception Se o elemento existir.
+	 */
+	public void inserir(int x) throws Exception {
+		raiz = inserir(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para inserir elemento.
+	 * @param x Elemento a ser inserido.
+	 * @param i No em analise.
+	 * @return No em analise, alterado ou nao.
+	 * @throws Exception Se o elemento existir.
+	 */
+	private No inserir(int x, No i) throws Exception {
+		if (i == null) {
+         i = new No(x);
+
+      } else if (x < i.elemento) {
+         i.esq = inserir(x, i.esq);
+
+      } else if (x > i.elemento) {
+         i.dir = inserir(x, i.dir);
+
+      } else {
+         throw new Exception("Erro ao inserir!");
+      }
+
+		return i;
+	}
+
+	/**
+	 * Metodo publico iterativo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	public void remover(int x) throws Exception {
+		raiz = remover(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @param i No em analise.
+	 * @return No em analise, alterado ou nao.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	private No remover(int x, No i) throws Exception {
+
+		if (i == null) {
+         throw new Exception("Erro ao remover!");
+
+      } else if (x < i.elemento) {
+         i.esq = remover(x, i.esq);
+
+      } else if (x > i.elemento) {
+         i.dir = remover(x, i.dir);
+
+      // Sem no a direita.
+      } else if (i.dir == null) {
+         i = i.esq;
+
+      // Sem no a esquerda.
+      } else if (i.esq == null) {
+         i = i.dir;
+
+      // No a esquerda e no a direita.
+      } else {
+         i.esq = antecessor(i, i.esq);
+		}
+
+		return i;
+	}
+
+	/**
+	 * Metodo para trocar no removido pelo antecessor.
+	 * @param i No que teve o elemento removido.
+	 * @param j No da subarvore esquerda.
+	 * @return No em analise, alterado ou nao.
+	 */
+	private No antecessor(No i, No j) {
+
+      // Existe no a direita.
+		if (j.dir != null) {
+         // Caminha para direita.
+			j.dir = antecessor(i, j.dir);
+
+      // Encontrou o maximo da subarvore esquerda.
+		} else {
+			i.elemento = j.elemento; // Substitui i por j.
+			j = j.esq; // Substitui j por j.ESQ.
+		}
+		return j;
+	}
+}
