aef0ecab3f878b9cb2dcff8c185f3f03434f2a65
Darsh Gandhi <32832454+darshmgandhi@users.noreply.github.com>
2020-10-17 14:57:50 +0530

    Added comments in LinearSearch.java for issue #512
    I had already contributed LinearSearch.java for issue #6. The pull request number is #617. Now adding descriptive comments in same file for issue #512 for hacktober.

--- LinearSearch.java
+++ LinearSearch.java
@@ -4,17 +4,19 @@ import java.util.Random;
 public class LinearSearch {
   
   public static void main(String[] args) {
-    Random rd = new Random();
+    //Taking random number of instead of user input
+    Random rd = new Random(); 
     int[] inputArray = new int[100];
     System.out.println("Array:");
     for (int i = 0; i < inputArray.length; i++) {
-      inputArray[i] = rd.nextInt(100000) + 1;
+      inputArray[i] = rd.nextInt(100000) + 1; 
       System.out.print(inputArray[i] + " ");
     }
     System.out.println("\nInput Integer to Search:");
     Scanner sc = new Scanner(System.in);
+    //Linear Search implementation starts here
     int searchKey = sc.nextInt();
-    for (int i = 0; i < inputArray.length; i++) {
+    for (int i = 0; i < inputArray.length; i++) { 
       if (inputArray[i] == searchKey) 
         System.out.println(searchKey + " found at index: " + i);
     }
