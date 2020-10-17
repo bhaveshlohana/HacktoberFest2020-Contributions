43413b0b9ed42f0c97686504af523efe6c1cfba9
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-10-17 12:17:41 +0530

    Add files via upload

---
+++ COVID-19 Tracker/build/precache-manifest.302b6eddbbe76e8d6da950ee7ec1b590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d86498d9481f4fb7a110cad26f867fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e906264c34c1fc902b24",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "568fa638fbe4f5d7dbc7",
+    "url": "/static/css/main.6ad8a1f2.chunk.css"
+  },
+  {
+    "revision": "e906264c34c1fc902b24",
+    "url": "/static/js/2.57331e50.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.57331e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568fa638fbe4f5d7dbc7",
+    "url": "/static/js/main.ffaf19df.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
