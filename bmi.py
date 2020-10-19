0f98c6089ef81e28c0e5075274ad88e405a209a3
leeyuchenxavier <52947079+leeyuchenxavier@users.noreply.github.com>
2020-10-19 13:57:15 +0800

    it calculates ur bmi for u ;]

---
+++ bmi.py
@@ -0,0 +1,3 @@
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height), 2))
