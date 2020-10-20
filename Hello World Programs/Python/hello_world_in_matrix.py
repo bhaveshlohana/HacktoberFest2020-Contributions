12a29e83a962d7101ca473a5d094503f2e95f36d
Kartikeya Gupta <55078693+Kartikeya006@users.noreply.github.com>
2020-10-20 17:14:30 +0530

    Python Hello world in Form of Matrix

---
+++ Hello World Programs/Python/hello_world_in_matrix.py
@@ -0,0 +1,6 @@
+r = int(input("Enter Number of Rows Of Matrix :-"))
+c = int(input("Enter Number of Columns of Matrix :-"))
+for i in range(r):
+    for j in range(c):
+        print("Hello World", end="\t\t")
+    print()
