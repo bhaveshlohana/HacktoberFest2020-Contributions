7c57ab0abd9a58e2ad24fdae486f1795e34cd6b1
Rahul R <31767308+RahulR19@users.noreply.github.com>
2020-10-21 19:09:16 +0530

    Updated Pig Latin Program

--- Projects/pig_latin.py
+++ Projects/pig_latin.py
@@ -1,23 +1,23 @@
 # Pig Latin Program
 
-ay = ‘ay’
-way = ‘way’
-consonant = (‘B’,’C’,’D’,’F’,’G’,’H’,’J’,’K’,’L’,’M’,’N’,’P’,’Q’,’R’,’S’,’T’,’Y’,’V’,’X’,’Z’)
-vowel = (‘A’,’E’,’I’,’O’,’U’)
-user_word = input(‘Enter a word to translate to Pig Latin: ‘)
+ay = 'ay'
+way = 'way'
+consonant = ('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','Y','V','X','Z')
+vowel = ('A','E','I','O','U')
+user_word = input('Enter a word to translate to Pig Latin: ')
 
 first_letter = user_word[0]
 first_letter = str(first_letter)
 first_letter=first_letter.upper()
 if first_letter in consonant:
-   print(first_letter,’is a consonant’)
+   print(first_letter,'is a consonant')
    length_of_word = len(user_word)
    remove_first_letter = user_word[1:length_of_word]
    pig_latin=remove_first_letter+first_letter+ay
-   print(‘The word in Pig Latin is:’,pig_latin)
+   print('The word in Pig Latin is:',pig_latin)
  elif first_letter in vowel:
-   print(first_letter,’is a vowel’)
+   print(first_letter,'is a vowel')
    pig_latin=user_word+way
-   print(‘The word in Pig Latin is:’,pig_latin)
+   print('The word in Pig Latin is:',pig_latin)
  else:
-   print(‘I dont know what’,first_letter,’is’)
+   print('I dont know what',first_letter,'is')
