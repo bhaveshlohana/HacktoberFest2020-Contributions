5862c734ca918785c9826067e227f614e7d91275
Arshita Narayan <49998029+arshita04@users.noreply.github.com>
2020-10-24 15:24:38 +0530

    play using capital letter keys
    Users tend to press caps lock key while playing this game and it tends to create unnecessary issues and hence I added an option to play using capital letters as well.

--- Games/Drum Kit/index.js
+++ Games/Drum Kit/index.js
@@ -48,6 +48,42 @@ function playsound(value){
       var audio = new Audio("sounds/kick-bass.mp3");
       audio.play();
       break;
+    
+    //Adding the option of capital letters as well
+    case "W":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "A":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "S":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "D":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "J":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "K":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "L":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
     default:console.log("not worked");
   }
 }
