39ee0fb4cbcb9f522800d5a6ba122acb32c373f4
manojpoddar <73521047+manojpoddar@users.noreply.github.com>
2020-10-27 18:38:21 +0530

    Hello World cpp

---
+++ Hello World Programs/C++/Hello World.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define lli long long int
+#define ull unsigned long long
+#define li long int
+#include<vector>
+using namespace std;
+int main()
+{
+ cout<<"Hello World !!"<<endl;
+ return 0;
+}
