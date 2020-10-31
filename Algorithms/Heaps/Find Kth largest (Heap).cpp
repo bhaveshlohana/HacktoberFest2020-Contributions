ddb1097d505abc5bd4b605b7a7de26e03f1a7330
Ram Sharma <65657087+chandresh189189189@users.noreply.github.com>
2020-10-31 15:17:55 +0530

    Add files via upload

---
+++ Algorithms/Heaps/Find Kth largest (Heap).cpp
@@ -0,0 +1,20 @@
+//Complexity < nlogn
+#include <bits/stdc++.h>
+using namespace std;
+
+int FindKth(int *a, int n, int k) {
+	int m[k];
+	for(int i=0;i<n;i++) {
+		
+	}
+}
+
+int main()
+{
+	   int a[] = {9,4,7,1,-2,6,5};
+	   int k = 2;
+	   int n = sizeof(a)/sizeof(a[0]);
+	   cout<<FindKth(a, n, k);
+	   return 0;
+}
+
